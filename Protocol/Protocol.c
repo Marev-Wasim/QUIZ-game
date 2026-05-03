5d2638d7fd9bd46a0b0cbf03c83bfc708cb4a614
Rana Samir <158211174+Rana554@users.noreply.github.com>
2026-05-04 01:08:57 +0300

    Add files via upload

--- Protocol/Protocol.c
+++ Protocol/Protocol.c
@@ -1,1 +1,109 @@
+#include "protocol.h"
+#include <string.h>
 
+void build_message(Message* msg, int type, const char* data) {
+    msg->type = type;
+    msg->length = strlen(data);
+    strncpy(msg->data, data, 1023);
+    msg->data[1023] = '\0';
+}
+
+int serialize(Message* msg, char* buffer) {
+
+    int type = htonl(msg->type);
+    int length = htonl(msg->length);
+
+    memcpy(buffer, &type, sizeof(int));
+    memcpy(buffer + sizeof(int), &length, sizeof(int));
+    memcpy(buffer + 2 * sizeof(int), msg->data, msg->length);
+
+    return 2 * sizeof(int) + msg->length;
+}
+
+int send_message(int sockfd, Message* msg) {
+    char buffer[2048];
+
+    int total_size = serialize(msg,buffer);
+
+    return writen(sockfd, buffer, total_size);
+}
+
+int recv_full(int sockfd, char *buffer, int length) {
+    int total = 0;
+    int bytes;
+
+    while (total < length) {
+        bytes = recv(sockfd, buffer + total, length - total, 0);
+
+        if (bytes <= 0) {
+            return -1; // error or connection closed
+        }
+
+        total += bytes;
+    }
+
+    return total;
+}
+
+int receive_message(int sockfd, Message* msg) {
+    char header[8];
+
+    // read header  (type + length)
+    if (recv_full(sockfd, header, 8) < 0)
+        return PROTO_ERR_DISCONNECT;
+
+    int type, length;
+
+    //deserialize part
+    memcpy(&type, header, 4);
+    memcpy(&length, header + 4, 4);
+
+    msg->type = ntohl(type);
+    msg->length = ntohl(length);
+
+    // validation
+    if (msg->length > 1024 || msg->length < 0)
+        return PROTO_ERR_LENGTH;   //corrupted message
+    if (msg->type < JOIN || msg->type > ERROR_MSG)
+        return PROTO_ERR_TYPE;
+
+    // read data
+    if (recv_full(sockfd, msg->data, msg->length) < 0)
+        return PROTO_ERR_RECV;
+
+    msg->data[msg->length] = '\0';
+
+    return PROTO_OK;
+}
+
+ssize_t writen(int fd, const void *vptr, size_t n)
+{
+    size_t
+    nleft;
+    ssize_t
+    nwritten;
+    const char
+    *ptr;
+    ptr = vptr;
+    nleft = n;
+    while (nleft > 0)
+    {
+        if ( (nwritten = write(fd, ptr, nleft)) <= 0)
+        {
+            if (nwritten < 0 && errno == EINTR)
+                nwritten = 0;
+            else
+                return(-1);
+        }
+        nleft -= nwritten;
+        ptr
+        += nwritten;
+    }
+    return(n);
+}
+
+void Writen(int fd, void *ptr, size_t nbytes)
+{
+    if (writen(fd, ptr, nbytes) != nbytes)
+        err_sys("writen error");
+}
---
+++ Protocol/Protocol.h
@@ -0,0 +1,37 @@
+#ifndef PROTOCOL_H_INCLUDED
+#define PROTOCOL_H_INCLUDED
+
+#include <string.h>
+#include <arpa/inet.h>
+
+//message types
+#define JOIN 1
+#define ASSIGN_ID 2
+#define START_GAME 3
+#define QUESTION 4
+#define ANSWER 5
+#define RESULT 6
+#define GAME_RESULT 7
+#define ERROR_MSG 8
+
+#define PROTO_OK                0
+#define PROTO_ERR_DISCONNECT   -1
+#define PROTO_ERR_LENGTH       -2
+#define PROTO_ERR_TYPE         -3
+#define PROTO_ERR_RECV         -4
+
+typedef struct {
+    int type;
+    int length;
+    char data[1024];
+} Message;
+
+void build_message(Message* msg, int type, const char* data) ;
+int serialize(Message* msg, char* buffer) ;
+int send_message(int sockfd, Message* msg) ;
+int recv_full(int sockfd, char *buffer, int length) ;
+int receive_message(int sockfd, Message* msg) ;
+ssize_t writen(int fd, const void *vptr, size_t n);
+void Writen(int fd, void *ptr, size_t nbytes);
+
+#endif // PROTOCOL_H_INCLUDED
