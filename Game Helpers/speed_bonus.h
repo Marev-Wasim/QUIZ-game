590d82a29bd31d4e56f81f39d4a9970586810326
Marev-Wasim <marevwasim79@gmail.com>
2026-05-08 16:44:06 +0300

    Add files via upload

---
+++ Game Helpers/speed_bonus.h
@@ -0,0 +1,21 @@
+#ifndef SPEED_BONUS_H
+#define SPEED_BONUS_H
+
+#include <sys/time.h>
+#include "player.h"
+
+#define BASE_POINTS 100
+#define MAX_BONUS 50
+#define QUESTION_TIMEOUT 10  //seconds allpwed per question
+
+typedef struct
+{
+    struct timeval strat_time;
+    struct timeval end_time;
+} QuizTimer;
+
+int calc_score (double response_time_sec);
+int cmp_players (const void* a, const void* b);
+void sort_leaderboard (Player* players, int num_players);
+
+#endif
